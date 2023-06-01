e5ca1ad7af3e7865fdd54a3cca8c428bc1800598
toroe33 <toroe33@yahoo.com>
2023-06-01 19:24:18 -0400

    Initial Commit

--- src/math_problems/FindLowestDifference.java
+++ src/math_problems/FindLowestDifference.java
@@ -13,8 +13,37 @@ public class FindLowestDifference {
         int[] array1 = {30, 12, 5, 9, 2, 20, 33, 1, -15};
         int[] array2 = {18, 25, 41, 47, 17, 36, 14, 19, -15};
 
+        System.out.println(findLowestDiffernce(array1, array2));
     }
 
+    public static int findLowestDifference(int[] array1, int[]array2){
 
+        ArrayList<Integer> arrayList1 = new ArrayList<>();
+        ArrayList<Integer> arrayList2 = new ArrayList<>();
+        ArrayList<Integer> arrayList3 = new ArrayList<>();
+        int lowest = Integer.MAX_VALUE;
+
+        for(int num : array1) {
+            arrayList1.add(num);
+        }
+        for(int num : array2){
+            arrayList2.add(num);
+        }
+
+        for(Integer num : arrayList1){
+            if(!(arrayList2.contains(num))){
+                arrayList3.add(num);
+            }
+        }
+
+        for(Integer num : arrayList3){
+            if(num < lowest){
+                lowest = num;
+            }
+        }
+
+        return lowest;
+
+    }
 
 }
