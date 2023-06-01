91f67895d67f80385752a43cc863c8ce079dfee2
toroe33 <toroe33@yahoo.com>
2023-06-01 19:32:46 -0400

    Initial Commit

--- src/math_problems/LowestNumber.java
+++ src/math_problems/LowestNumber.java
@@ -11,6 +11,17 @@ public class LowestNumber {
         int[] array = new int[] {211, 110, 99, 34, 67, 89, 67, 456, 321, 456, 78, 90, 45, 32, 56, 78, 90, 54, 32, 123,
                 67, 5, 679, 54, 32, 65};
 
+        int minValue = array[0];
+
+        for (int i : array) {
+
+            if (i < minValue)
+
+                minValue = i;
+        }
+
+        System.out.println("Lowest number in array: " + minValue);
+
 
     }
 
