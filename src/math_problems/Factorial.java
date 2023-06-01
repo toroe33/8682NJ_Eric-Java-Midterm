5f46445f941b8dcc89942ed62b33ce4b5d98fb5b
toroe33 <toroe33@yahoo.com>
2023-06-01 19:05:50 -0400

    Initial Commit

--- src/math_problems/Factorial.java
+++ src/math_problems/Factorial.java
@@ -10,7 +10,28 @@ public class Factorial {
      * HINT: Factorial of 5! = 5 x 4 X 3 X 2 X 1 = 120
      */
 
+
+    static int factorialRecursion(int n) {
+        if (n == 0)
+            return 1;
+
+        return n * factorialRecursion(n - 1);
+    }
+
+    static int factorialRecursion(int n)
+    {
+        int res = 1, i;
+        for (i = 2; i <= n; i++)
+            res *= i;
+        return res;
+    }
+
     public static void main(String[] args) {
+        {
+            int num = 5;
+            System.out.println("Factorial of " + num + " is " + factorialRecursion(5));
+            System.out.println("Factorial of " + num + "is" + factorialIteration(5));
+        }
 
 
     }
