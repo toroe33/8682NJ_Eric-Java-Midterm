84bb4fa77226a24e895978cde9313e638705b5d6
toroe33 <toroe33@yahoo.com>
2023-06-01 19:13:04 -0400

    Initial Commit

--- src/math_problems/Fibonacci.java
+++ src/math_problems/Fibonacci.java
@@ -8,7 +8,20 @@ public class Fibonacci {
      * e.g. - 0,1,1,2,3,5,8,13
      **/
 
+    static int fibonacci(int n) {
+        if (n <= 1)
+            return n;
+
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+
     public static void main(String[] args) {
+
+        int N = 40;
+
+        for (int i = 0; i < N; i++) {
+            System.out.print(fibbonacci(i) + " ");
+        }
 
     }
 }
