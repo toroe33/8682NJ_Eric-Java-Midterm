eb19aa21eea47cd0f9127af850d359b017f7dd69
toroe33 <toroe33@yahoo.com>
2023-06-01 20:12:54 -0400

    Initial Commit

--- src/math_problems/PrimeNumber.java
+++ src/math_problems/PrimeNumber.java
@@ -10,7 +10,27 @@ public class PrimeNumber {
      */
 
 
+    static boolean isPrime(int n) {
+        if (n <= 1)
+            return false;
+
+        for (int i = 2; i < n; i++)
+            if (n % i == 0)
+                return false;
+
+        return true;
+    }
+    static void primeNumber(int n)
+    {
+        for (int i = 2; i <= n; i++)
+        {
+            if (isPrime(i))
+                System.out.print(i + " ");
+        }
+    }
     public static void main(String[] args) {
+        int n = 1000000;
+        primeNumber(n);
 
     }
 
