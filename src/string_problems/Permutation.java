c0e2f872a7895d0b1f3630029fa1445f908f905b
toroe33 <toroe33@yahoo.com>
2023-06-01 21:36:49 -0400

    Initial Commit

--- src/string_problems/Permutation.java
+++ src/string_problems/Permutation.java
@@ -8,8 +8,27 @@ public class Permutation {
      * e.g. -  "ABC" = "ABC" "ACB" "BAC" "BCA" "CAB" "CBA"
      */
 
+
+    static void permutation(String str, String ans) {
+
+        if (str.length() == 0) {
+            System.out.print(ans + " ");
+            return;
+        }
+        for (int i = 0; i < str.length(); i++) {
+
+            char ch = str.charAt(i);
+
+            String ros = str.substring(0, i) + str.substring(i + 1);
+
+            permutation(ros, ans + ch);
+        }
+    }
+
     public static void main(String[] args) {
+        String s = "ABC";
+        permutation(s, "");
 
     }
-    // Implement Here
+
 }
